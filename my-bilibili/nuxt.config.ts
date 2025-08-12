89077fb98c025bf0951b72a4bca581eb307635d1
zk <yourname@example.com>
2025-08-12 17:16:53 +0800

    init

---
+++ my-bilibili/nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // 是否开启服务端渲染,默认开启
+  ssr: true, 
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
